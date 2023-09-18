e9034952c6b79535c310ccaf32d10d66253075d3
juliawu <xiaohanwu666@gmail.com>
2023-09-18 23:27:33 +0200

    cleaned gapminder data

---
+++ data_clean.R
@@ -0,0 +1,17 @@
+# Set-up your script ------------------------------------------------------
+
+#install.packages(c("tidyverse", "gapminder", "pacman"))
+pacman::p_load(tidyverse, gapminder)
+
+# Load your Data into R ---------------------------------------------------
+
+data(gapminder)
+head(gapminder)
+
+# Clean your Data ---------------------------------------------------------
+
+gapminder_clean <- gapminder %>% 
+  rename(life_exp = lifeExp, gdp_per_cap = gdpPercap) %>% 
+  mutate(gdp = pop * gdp_per_cap)
+
+head(gapminder_clean)
